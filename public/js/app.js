0c40ccc3769e3085ab8dc1988705af1db3a1c9ed
Jack Zhang <15jzhang@gmail.com>
2016-03-06 00:48:01 -0500

    Complete image input view

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,22 @@
 
 $(document).ready(function() {
-    
+    loadImage();
+    imageError();
 });
+
+function loadImage() {
+    $("#load-image").on('click', function(){
+        $("#selected-image").attr("src", $("#url-input").val());
+        console.log($("#url-input").val());
+    });
+
+    $('#selected-image').on({
+        load: function(){
+            console.log("Continue to the select color view.");
+        },
+        error: function(){
+            console.log("Invalid image url.");
+            $('p.url-input-error').css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1}, 'slow');
+        }
+    })
+}
