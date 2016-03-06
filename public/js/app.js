827246584d285f940a5e181a64bdcc672bcaaacc
Jack Zhang <15jzhang@gmail.com>
2016-03-06 01:50:01 -0500

    Complete functionality for image inputs using JavaScript FileReader

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,37 @@
 
 $(document).ready(function() {
-    loadImage();
-    imageError();
+    // Event handlers
+    loadImageUrl();
+    continueToSelectColor();
 });
 
-function loadImage() {
+function loadImageUrl() {
     $("#load-image").on('click', function(){
         $("#selected-image").attr("src", $("#url-input").val());
         console.log($("#url-input").val());
     });
+}
 
+function loadImageFile() {
+    // FileReader support
+    if (FileReader) {
+        var display = document.querySelector('#selected-image');
+        var file = document.querySelector('#image-input').files[0];
+        var reader = new FileReader();
+
+        reader.addEventListener("load", 
+            function () {
+                display.src = reader.result;
+            },
+        false);
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    }
+}
+
+function continueToSelectColor() {
     $('#selected-image').on({
         load: function(){
             console.log("Continue to the select color view.");
