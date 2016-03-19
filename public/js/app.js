7a5c192e51a29da80e85de38ed40fd35929821e1
Jack Zhang <15jzhang@gmail.com>
2016-03-19 01:45:28 -0400

    Rewrite image loading for HTML5 canvas

--- public/js/app.js
+++ public/js/app.js
@@ -2,25 +2,54 @@
 $(document).ready(function() {
     // Event handlers
     loadImageUrl();
-    continueToSelectColor();
 });
 
 function loadImageUrl() {
-    $("#load-image").on('click', function(){
-        $("#selected-image").attr("src", $("#url-input").val());
+    $("#load-image").on('click', function() {
+        // Set up canvas
+        var canvas = document.querySelector('#select-color-canvas');
+        var context = canvas.getContext('2d');
+
+        // Instantiate JS image object, assign input url
+        var imageObj = new Image;
+        imageObj.src = $("#url-input").val();
+
+        // Load valid image urls, continue to select color view
+        imageObj.onload = function(){
+            context.drawImage(imageObj,0,0);
+            console.log("Valid image url. Continue to selector view");
+        };
+
+        // Display error message for invalid image urls
+        imageObj.onerror = function(){
+            if ($("#url-input").val() !== '') {
+                $('p.url-input-error').css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1}, 'slow');
+                console.log("Invalid image url.");
+            }
+        };
     });
 }
 
+// Function triggered by #image-input file input element
 function loadImageFile() {
     // FileReader support
     if (FileReader) {
-        var display = document.querySelector('#selected-image');
+        // Set up canvas
+        var canvas = document.querySelector('#select-color-canvas');
+        var context = canvas.getContext('2d');
+
+        // Set up file reader
         var file = document.querySelector('#image-input').files[0];
         var reader = new FileReader();
 
+        // Instantiate JS image object
+        var imageObj = new Image;
+
+        // Load file src into imageObj, draw imageObj onto canvas
         reader.addEventListener("load", 
             function () {
-                display.src = reader.result;
+                imageObj.src = reader.result;
+                context.drawImage(imageObj,0,0);
             },
         false);
 
@@ -30,19 +59,4 @@ function loadImageFile() {
     } else {
         console.log('Javascript File API not supported.');
     }
-}
-
-function continueToSelectColor() {
-    $('#selected-image').on({
-        load: function(){
-            console.log("Continue to the select color view.");
-
-        },
-        error: function(){
-            if ($("#url-input").val() !== '') {
-                console.log("Invalid image url.");
-                $('p.url-input-error').css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1}, 'slow');
-            }
-        }
-    })
 }
